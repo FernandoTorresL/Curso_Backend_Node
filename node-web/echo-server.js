65669c0c5fa79a1629e94fa10f29b06bdedfd416
Fernando Torres <fertorresmx@gmail.com>
2019-10-24 13:08:48 -0500

    Solución al reto de curso node js, video 08: Node.js para la web

--- node-web/echo-server.js
+++ node-web/echo-server.js
@@ -9,8 +9,28 @@ server.on('request', (req, res) => {
         })
         .on('end', () => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
+
             body = Buffer.concat(body).toString();
-            res.end(body);
+            //Hay que incluir el uso horario, en mi caso para México -6hrs desde UTC
+            fechaEntrada = body + 'T12:00:00-06:00';
+
+            let fechaCumple = new Date(fechaEntrada);
+            if (fechaCumple instanceof Date && !isNaN(fechaCumple.valueOf())) {
+                console.log(`\nFecha válida: ${body} `);
+                console.log(`Fecha de nacimiento en horario local: ${fechaCumple}`);
+
+                //Obtenemos el día de la semana
+                let dayOfWeek = 
+                    ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+                let message = dayOfWeek[fechaCumple.getDay()];
+                console.log(`El día de tu nacimiento fue: ${message}`);
+
+                res.end(message);
+            } else {
+                console.log(`\nFecha inválida: ${body}. Por favor usa formato 'YYYY-MM-DD'`);
+                res.statusCode = 404;
+                res.end('FECHA INVÁLIDA');
+            }
         })
     } else {
         res.statusCode = 404;
