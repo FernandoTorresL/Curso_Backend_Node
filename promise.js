6534fccb2869bf8930421f1cbc43e7875fba5e1f
Fer Torres <fertorresmx@gmail.com>
2019-10-24 00:53:28 -0500

    Add async-await example

--- promise.js
+++ promise.js
@@ -8,6 +8,7 @@ const promise = new Promise ((resolve, reject) => {
     }, 2000);
   });
 
-promise.then(msg => msg.toUpperCase())
-.then(msg => console.log('message', msg))
-.catch(err => console.log('error', err));
+promise
+    .then(msg => msg.toUpperCase())
+    .then(msg => console.log('message', msg))
+    .catch(err => console.log('error', err));
