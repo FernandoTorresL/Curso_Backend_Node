861131068c811220c1d40874907f48a15d961b53
Fernando Torres <fertorresmx@gmail.com>
2019-10-25 10:54:16 -0500

    Using a server to read stream/file. Analizing memory use

--- streams/big-file.js
+++ streams/big-file.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const file = fs.createWriteStream('./big');
 
 for(let i = 0; i <= 1e6; i++) {
-    file.write('Estaba la pájara pinta sentada en un verde limón. Con el pico cortaba la rama, con la rama cortaba la flor');
+    file.write('Estaba la pájara pinta sentada en un verde limón. Con el pico cortaba la rama, con la rama cortaba la flor. Estaba la pájara pinta sentada en un verde limón. Con el pico cortaba la rama, con la rama cortaba la flor. Estaba la pájara pinta sentada en un verde limón. Con el pico cortaba la rama, con la rama cortaba la flor.');
 }
 
 file.end();
